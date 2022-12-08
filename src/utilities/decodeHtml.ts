dced2ca7e4d6e6c322b973b0601ff8f67df5add1
Souvlaki42 <souvlaki420@gmail.com>
2022-12-08 23:31:48 +0200

    New ReactTS Version 2

--- src/utilities/decodeHtml.ts
+++
@@ -1,5 +0,0 @@
-export default function (rawText: string) {
-    const textarea = document.createElement("textarea");
-    textarea.innerHTML = rawText;
-    return textarea.value;
-}
--- src/utilities/randomUniqueItem.ts
+++
@@ -1,5 +0,0 @@
-export default function (array: string[]) {
-    const options = Math.floor(Math.random() * array.length);
-    const removed = array.splice(options, 1);
-    return removed[0];
-}
